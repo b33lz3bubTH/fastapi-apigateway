c191bbc3c837eac9a58e00e49e5b55b5c6dc1ee0
Sourav <souravsunju@gmail.com>
2021-02-17 13:42:29 +0530

    Media Push [File Size 3MB MAX]

--- app.py
+++ app.py
@@ -10,7 +10,29 @@ from endpoint_definations import endpoint_definations
 from config import config
 import jwt
 
+from starlette import status
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.types import ASGIApp
+
+
+class LimitUploadSize(BaseHTTPMiddleware):
+    def __init__(self, app: ASGIApp, max_upload_size: int) -> None:
+        super().__init__(app)
+        self.max_upload_size = max_upload_size
+
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        if request.method == 'POST':
+            if 'content-length' not in request.headers:
+                return Response(status_code=status.HTTP_411_LENGTH_REQUIRED)
+            content_length = int(request.headers['content-length'])
+            if content_length > self.max_upload_size:
+                return Response(status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE)
+        return await call_next(request)
+
 app = FastAPI(debug=True)
+app.add_middleware(LimitUploadSize, max_upload_size=3_000_000)
 
 routerPaths = getRoutes()
 for routerPath in routerPaths:
